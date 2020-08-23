6f0747ff86d2f50c8c289847dc2fc4252b3da689
Peter Stoenchev <peter.stoenchev@abv.bg>
2020-08-23 23:01:16 +0300

    Initial Commit

---
+++ Geometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Geometry
+//
+//  Created by Mac on 23.08.20.
+//  Copyright © 2020 peter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
